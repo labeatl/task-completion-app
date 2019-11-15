423c1935629b73764b977c15f2f12075e3b72880
Elias Fandi <16519982+elditing@users.noreply.github.com>
2019-11-15 22:46:08 +0000

    Merge old login-page with new

--- frontend dev/lib/pages/login-page.dart
+++ frontend dev/lib/pages/login-page.dart
@@ -84,16 +84,18 @@ class _LoginPageState extends State<LoginPage> {
                       style: TextStyle(fontSize: 17),
                     ),
                     onPressed: () {
+                      //The below strings store the form data entered.
+                      String name, surName, email, password, confirmPassword;
                       showDialog(
                           context: context,
                           builder: (BuildContext context) {
                             return AlertDialog(
                               content: Form(
                                 key: _formKey,
-                                child: new Column(
+                                child: Column(
                                   mainAxisSize: MainAxisSize.min,
                                   children: <Widget>[
-                                    new TextFormField(
+                                    TextFormField(
                                       decoration:
                                       InputDecoration(labelText: "name"),
                                       validator: (value) {
@@ -102,23 +104,30 @@ class _LoginPageState extends State<LoginPage> {
                                         }
                                         return null;
                                       },
+                                      onSaved:(String val){
+                                        name = val;
+                                      },
                                     ),
                                     Padding(
                                       padding: EdgeInsets.all(10.0),
-                                      child: new TextFormField(
+                                      child: TextFormField(
                                         decoration: InputDecoration(
                                             labelText: "surname"),
                                         validator: (value) {
                                           if (value.isEmpty) {
                                             return 'Please enter some text';
                                           }
+
                                           return null;
+                                        },
+                                        onSaved:(String val){
+                                          surName = val;
                                         },
                                       ),
                                     ),
                                     Padding(
                                       padding: EdgeInsets.all(10.0),
-                                      child: new TextFormField(
+                                      child: TextFormField(
                                         decoration: InputDecoration(
                                             labelText: "email address"),
                                         validator: (value) {
@@ -127,17 +136,72 @@ class _LoginPageState extends State<LoginPage> {
                                           }
                                           return null;
                                         },
+                                        onSaved:(String val){
+                                          email = val;
+                                        },
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.all(10.0),
+                                      child: TextFormField(
+                                        obscureText: true,
+                                        //Make this input hidden
+                                        decoration: InputDecoration(
+                                            labelText: "password"),
+                                        validator: (value) {
+                                          password = value;
+                                          if (value.isEmpty) {
+                                            return 'Please enter some text';
+                                          }
+                                          return null;
+                                        },
+                                        onSaved:(String val){
+                                          password = val;
+                                        },
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.all(10.0),
+                                      child: TextFormField(
+                                        obscureText: true,
+                                        //Make this input hidden
+
+                                        decoration: InputDecoration(
+                                            labelText: "Confirm Password"),
+                                        validator: (value) {
+                                          if (value.isEmpty) {
+                                            return 'Please enter some text';
+                                          }
+                                          else if (value.compareTo(password) != 0) {
+                                            return 'Passwords do not match';
+                                          }
+                                          return null;
+                                        },
+                                        onSaved:(String val){
+                                          confirmPassword = val;
+                                        },
                                       ),
                                     ),
                                     Padding(
                                       padding: const EdgeInsets.all(10.0),
-                                      child: new RaisedButton(
-                                        child: new Text("Submit"),
+                                      child: RaisedButton(
+                                        child: Text("Submit"),
                                         onPressed: () {
                                           if (_formKey.currentState
-                                              .validate()) {
+                                              .validate()){
                                             _formKey.currentState.save();
+
+                                            var jsonData = {
+                                              'name':  name,
+                                              'surName':  surName,
+                                              'email':  email,
+                                              'password': password
+
+                                            };
+
+
                                           }
+
                                         },
                                       ),
                                     )
