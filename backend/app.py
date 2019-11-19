5b134c4b2255b05b4df603b90c1bf68174941b3a
Elias Fandi <16519982+elditing@users.noreply.github.com>
2019-11-19 06:35:34 +0000

    Reimplement signup and implement login

--- backend/app.py
+++ backend/app.py
@@ -2,34 +2,37 @@ from flask import Flask, jsonify
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
-
+api = Api(app)
 #Database connection information
-dbParam = 'mysql+pymysql://root@localhost/taskr'
 
+dbParam = 'mysql+pymysql://root@localhost/taskr'
+app.config['SQLALCHEMY_DATABASE_URI'] = dbParam
+if __name__ == "__main__":
+    app.run(host='192.168.137.1')
 #Flask secret key
 theKey = 'thEejrdaR5$wE3yY4wsehn4wASHR'
 
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 class Accounts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    surName = db.Column(db.String)
-    email = db.Column(db.String)
-    password = db.Column(db.String)
+    name = db.Column(db.String(20))
+    surName = db.Column(db.String(25))
+    email = db.Column(db.String(40))
+    password = db.Column(db.String(100))
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-@app.route('/signup', methods['GET','POST'])
-def usr_signup():
-    
-    if request.method == 'POST':
-    #implement  'on_json_loading_failed()'
-        signUpData =  request.get_json()
+class hello(Resource):
+    def get(self):
+        return 'Bruh'
+api.add_resource(hello, '/')
+class UserSignUp(Resource):
+    def put(self):
+        #request.form("data")
+        signUpData = request.get_json()
 
         #signUpData['name']
         name = signUpData['name']
@@ -51,15 +54,35 @@ def usr_signup():
 
         else: 
             status = 'failed'
-    else: 
-        status = jsonify('failed')
+        return status
 
 
+api.add_resource(UserSignUp, '/signup')
 
-    return status
+  
+class UserLogin(Resource):
+    def get():
+        #request.form("data")
+        loginData = request.get_json()
 
-@app.route('/login')
-def usr_login():
-    return 0
+        #signUpData['name']
+        usrEmail = loginData['email']
+        unhashedPassword = signUpData['password']
+        #check password is same on frontend
+        hashedPassword = generate_password_hash(unhashedPassword)
 
+        #Make sure the email is not already taken
+        if Accounts.query.filter_by(email=usrEmail,password=hashedPassword).first() is  None:
+            #Login and send token
+            
+            
+
+            #Add account to the database
+            status = 'success'
+        else: 
+            status = 'failed'
+        return status
+
+
+api.add_resource(UserLogin, '/login')
 
