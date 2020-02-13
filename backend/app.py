d0a1c56ed1bb5720247e21e642ba2faea9918826
Elias Fandi <16519982+elditing@users.noreply.github.com>
2020-02-13 18:46:39 +0000

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -210,7 +210,7 @@ class AddUserSkill(Resource):
         #if User_Skills.query.filter_by(id=usrid).first() is None:
         #addskill = Accounts.skills.append(id_user=usrid, id=skillid)
         #Accounts.append(id_user=usrid, id=skillid)
-        theUser = Accounts.query.filter_by.first(id_user=usrid)
+        theUser = Accounts.query.filter_by(id_user=usrid).first()
         theSkill = Skills.query.filter_by(id=skillid)
         theUser.skills.append(theSkill)
         # Add account to the database
