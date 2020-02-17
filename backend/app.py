b8962859df3598a558b25db6fb9b8797c9fbb25c
labeatl <labeatlatifi1@gmail.com>
2020-02-17 17:00:15 +0000

    s

--- backend/app.py
+++ backend/app.py
@@ -240,7 +240,7 @@ api.add_resource(AddUserSkill, '/adduserskill')
 
 class GetUserSkills(Resource):
     def get(self):
-        userSkills = Accounts.query.filter_by(id_user=1)
+        userSkills = user_skills.query.filter_by(id_user=1)
         list = userSkills.all()
         print(list)
         skillList = []
