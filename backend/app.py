e73b27f1b33e9051ef9a126af7c6c4018f2ade44
Elias Fandi <elias@fandi.me>
2020-02-28 21:40:31 +0000

    change confirm email http request method

--- backend/app.py
+++ backend/app.py
@@ -415,7 +415,7 @@ class PasswordResetRequest(Resource):
 api.add_resource(PasswordResetRequest, "/resetpassword")
 
 class ConfirmEmail(Resource):
-    def post(self):
+    def get(self):
         resetID = reset_id
         decoded = s.loads(resetID)
         confirmUser = Accounts.query.filter_by(id_user=decoded).first()
