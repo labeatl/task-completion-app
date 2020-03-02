4306a6435c1ed5c3d22530cf40183910adf43012
Elias Fandi <elias@fandi.me>
2020-03-02 23:58:25 +0000

    Encode dictionary

--- backend/app.py
+++ backend/app.py
@@ -179,7 +179,7 @@ api.add_resource(TasksAdded, '/addtask')
 
 
 def generate_token(id,expiration=86400):
-    token = b.dumps(id)
+    token = b.dumps({ 'id': id })
     #token  = token.decode('utf8').replace("'", '"')
     return token
 
