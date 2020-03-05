4a6c667620c93ff047666feaf588b38f5b929b0a
labeatl <labeatlatifi1@gmail.com>
2020-03-05 11:54:24 +0000

    s

--- backend/app.py
+++ backend/app.py
@@ -173,10 +173,7 @@ class TasksAdded(Resource):
         Location = request.form['location']
         Picture = request.form['picture']
         owner = Accounts.query.filter_by(id_user=4).first()
-        createTask = Tasks(title=Title, description=Description, category=Category, et=Et, price=Price, location=Location, picture=Picture, owner_id=owner)
-        print(createTask)
-        print(owner)
-        print(owner.id_user)
+        createTask = Tasks(title=Title, description=Description, category=Category, et=Et, price=Price, location=Location, picture=Picture, owner_id=owner.id_user)
         db.session.add(createTask)
         db.session.commit()
 
