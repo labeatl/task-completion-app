76a92773215d23b12865af11d2b397b9e641fad8
labeatl <labeatlatifi1@gmail.com>
2020-05-01 21:43:06 +0100

    s

--- backend/app.py
+++ backend/app.py
@@ -650,12 +650,12 @@ def administration():
 
 
 class GetName(Resource):
+    print("bllablla")
     def get(self):
         user = Accounts.query.filter_by(id_user=1).first()
         print(user)
         user_name= user.name
         print(user)
-        
         return user_name
 
 api.add_resource(GetName, '/getName')
